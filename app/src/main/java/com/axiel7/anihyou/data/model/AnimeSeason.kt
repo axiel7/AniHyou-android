d47a55684d9ee8abd112aa60496523027fcb36c5
Axel <axiel7lopez@gmail.com>
2023-05-13 10:13:59 +0900

    Make AnimeSeason localizable

--- app/src/main/java/com/axiel7/anihyou/data/model/AnimeSeason.kt
+++ app/src/main/java/com/axiel7/anihyou/data/model/AnimeSeason.kt
@@ -3,15 +3,16 @@ package com.axiel7.anihyou.data.model
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.axiel7.anihyou.R
+import com.axiel7.anihyou.data.model.base.Localizable
 import com.axiel7.anihyou.type.MediaSeason
 
 data class AnimeSeason(
     var year: Int,
     var season: MediaSeason
-)
-
-@Composable
-fun AnimeSeason.localized() = "${season.localized()} $year"
+) : Localizable {
+    @Composable
+    override fun localized() = "${season.localized()} $year"
+}
 
 @Composable
 fun MediaSeason.localized() = when (this) {
