390b28104d8e2a2a3bab654c6ac01c12a3d99911
Axel <axiel7lopez@gmail.com>
2023-05-19 21:27:45 +0900

    Add text field to score slider

--- app/src/main/java/com/axiel7/anihyou/ui/composables/media/SliderRatingView.kt
+++ app/src/main/java/com/axiel7/anihyou/ui/composables/media/SliderRatingView.kt
@@ -3,9 +3,13 @@ package com.axiel7.anihyou.ui.composables.media
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Surface
@@ -18,6 +22,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.axiel7.anihyou.R
@@ -37,20 +43,21 @@ fun SliderRatingView(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = painterResource(R.drawable.star_24),
-                contentDescription = "star",
-                tint = MaterialTheme.colorScheme.onSurfaceVariant
-            )
-            Text(
-                text = if (showAsDecimal) String.format("%.1f", rating)
-                else String.format("%.0f", rating),
-                color = MaterialTheme.colorScheme.onSurfaceVariant
-            )
-        }
+        OutlinedTextField(
+            value = if (rating == 0.0) ""
+            else if (showAsDecimal) String.format("%.1f", rating)
+            else String.format("%.0f", rating),
+            onValueChange = { value ->
+                value.toDoubleOrNull()?.let {
+                    if (it <= maxValue) rating = it
+                }
+            },
+            modifier = Modifier.width(128.dp),
+            label = { Text(text = stringResource(R.string.score)) },
+            suffix = { Text(text = "/${String.format("%.0f", maxValue)}") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
+        )
+
         Slider(
             value = rating.toFloat(),
             onValueChange = { rating = it.toDouble() },
@@ -70,7 +77,7 @@ fun SliderRatingViewPreview() {
         Surface {
             SliderRatingView(
                 maxValue = 100.0,
-                showAsDecimal = true,
+                showAsDecimal = false,
                 onRatingChanged = {}
             )
         }
