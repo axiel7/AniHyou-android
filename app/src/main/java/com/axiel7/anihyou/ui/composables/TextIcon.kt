2fcb4328e480f2d26d511db33370d0c8b3e52eb3
Axel <axiel7lopez@gmail.com>
2023-05-25 10:22:22 +0900

    Fix TextSubtitleVertical alignment

--- app/src/main/java/com/axiel7/anihyou/ui/composables/TextIcon.kt
+++ app/src/main/java/com/axiel7/anihyou/ui/composables/TextIcon.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -125,12 +126,15 @@ fun TextSubtitleVertical(
     ) {
         Text(
             text = text ?: stringResource(R.string.unknown),
-            color = MaterialTheme.colorScheme.onSurface
+            color = MaterialTheme.colorScheme.onSurface,
+            textAlign = TextAlign.Center,
         )
         Text(
             text = subtitle,
             color = MaterialTheme.colorScheme.outline,
-            fontSize = 13.sp
+            fontSize = 13.sp,
+            textAlign = TextAlign.Center,
+            lineHeight = 15.sp
         )
     }
 }
