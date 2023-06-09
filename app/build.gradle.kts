f1650d69a07e166ef2dc7a2f304525626d8a5f9e
Axel <axiel7lopez@gmail.com>
2023-06-09 19:33:37 +0900

    Threads: add support for images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,6 +111,8 @@ dependencies {
     implementation("com.apollographql.apollo3:apollo-runtime:$apolloVersion")
     implementation("com.apollographql.apollo3:apollo-normalized-cache:$apolloVersion")
 
+    implementation("com.github.jeziellago:compose-markdown:0.3.3")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
