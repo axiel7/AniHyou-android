f1562b95d523fb7b988557a3f70551adc3de1bc0
Axel <axiel7lopez@gmail.com>
2023-06-09 16:15:43 +0900

    Update navigation 2.7.0-beta01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,7 +96,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
-    implementation("androidx.navigation:navigation-compose:2.7.0-alpha01")
+    implementation("androidx.navigation:navigation-compose:2.7.0-beta01")
     implementation("androidx.glance:glance-appwidget:1.0.0-beta01")
 
     val accompanistVersion = "0.30.1"
