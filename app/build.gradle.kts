5914c8877bd3760d321ee13d54b948841680c31a
Axel <axiel7lopez@gmail.com>
2023-06-12 15:20:02 +0900

    Update compose bom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,7 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.8.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
 
-    implementation(platform("androidx.compose:compose-bom:2023.05.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
