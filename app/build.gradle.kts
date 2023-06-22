eec0f9da93d76622a3bdfe44b00960b48dc9dc15
Axel <axiel7lopez@gmail.com>
2023-06-22 12:30:00 +0900

    1.1.0-beta04

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.axiel7.anihyou"
         minSdk = 23
         targetSdk = 33
-        versionCode = 16
-        versionName = "1.1.0-beta03"
+        versionCode = 17
+        versionName = "1.1.0-beta04"
         archivesName.set("anihyou-$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
