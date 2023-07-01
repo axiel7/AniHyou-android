d774aa6bb10bc5db00a9632daace0b6664dc57cd
axiel7 <axiel7lopez@gmail.com>
2023-07-01 16:58:06 +0900

    Update compose navigation 2.7.0-beta02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,7 +103,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.2.0-alpha03")
 
-    implementation("androidx.navigation:navigation-compose:2.7.0-beta01")
+    implementation("androidx.navigation:navigation-compose:2.7.0-beta02")
     implementation("androidx.glance:glance-appwidget:1.0.0-beta01")
 
     val accompanistVersion = "0.30.1"
