af913bef5a6dbe0d206888bb340e6d4560796e57
axiel7 <axiel7lopez@gmail.com>
2023-07-06 16:25:49 +0900

    Optimize imports

--- app/src/main/java/com/axiel7/anihyou/ui/screens/explore/search/GenresTagsSheet.kt
+++ app/src/main/java/com/axiel7/anihyou/ui/screens/explore/search/GenresTagsSheet.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
--- app/src/main/java/com/axiel7/anihyou/ui/screens/mediadetails/edit/EditMediaSheet.kt
+++ app/src/main/java/com/axiel7/anihyou/ui/screens/mediadetails/edit/EditMediaSheet.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
