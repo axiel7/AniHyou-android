46de00f1e65d77b73b193a21f7071f7919cddbc5
axiel7 <axiel7lopez@gmail.com>
2023-07-30 12:26:18 +0900

    optimize imports

--- app/src/main/java/com/axiel7/anihyou/ui/screens/usermedialist/UserMediaListView.kt
+++ app/src/main/java/com/axiel7/anihyou/ui/screens/usermedialist/UserMediaListView.kt
@@ -6,13 +6,10 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
