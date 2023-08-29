b70cfcd09d992023a9a806060b5e1c24b6e58cc5
axiel7 <axiel7lopez@gmail.com>
2023-08-29 12:38:48 +0900

    fix icon button using material 2

--- app/src/main/java/com/axiel7/anihyou/ui/composables/CommonIconButton.kt
+++ app/src/main/java/com/axiel7/anihyou/ui/composables/CommonIconButton.kt
@@ -1,8 +1,8 @@
 package com.axiel7.anihyou.ui.composables
 
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
