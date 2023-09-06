bed4379a6ad70c7a4f7d6c9c07ab5fbbc6befe69
axiel7 <axiel7lopez@gmail.com>
2023-09-06 12:07:36 +0900

    1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.axiel7.anihyou"
         minSdk = 23
         targetSdk = 34
-        versionCode = 25
-        versionName = "1.1.6"
+        versionCode = 26
+        versionName = "1.1.7"
         archivesName.set("anihyou-$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
