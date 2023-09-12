f3b3da6dfd038aa6e063e64feff72b30ca0c1b3f
axiel7 <axiel7lopez@gmail.com>
2023-09-12 19:24:35 +0900

    constant regex variables

--- app/src/main/java/com/axiel7/anihyou/utils/MarkdownUtils.kt
+++ app/src/main/java/com/axiel7/anihyou/utils/MarkdownUtils.kt
@@ -8,11 +8,14 @@ object MarkdownUtils {
     private const val ANIHYOU_IMAGE_SCHEME = "anihyouimage"
     private const val ANIHYOU_SPOILER_SCHEME = "anihyouspoiler://"
 
+    private val imageRegex = Regex("(img\\d*%*)\\((.*)\\)")
+    private val spoilerRegex = Regex("~!(.*)!~")
+
     fun String.formatImageTags() =
-        replace(Regex("(img\\d*%*)\\((.*)\\)"), "\n[View image]($ANIHYOU_IMAGE_SCHEME\$2)\n")
+        replace(imageRegex, "\n[View image]($ANIHYOU_IMAGE_SCHEME\$2)\n")
 
     fun String.formatSpoilerTags() =
-        replace(Regex("~!(.*)!~")) {
+        replace(spoilerRegex) {
             val spoilerEncoded = URLEncoder.encode(it.groupValues[1], "UTF-8")
             "\n[View spoiler]($ANIHYOU_SPOILER_SCHEME$spoilerEncoded)\n"
         }
