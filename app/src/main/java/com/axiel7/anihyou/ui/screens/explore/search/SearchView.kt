5f808e8b13518a8352cb93a08c10372ff9d79660
axiel7 <axiel7lopez@gmail.com>
2023-10-19 09:33:25 +0900

    search: save filter show state

--- app/src/main/java/com/axiel7/anihyou/ui/screens/explore/search/SearchView.kt
+++ app/src/main/java/com/axiel7/anihyou/ui/screens/explore/search/SearchView.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -66,7 +67,7 @@ fun SearchView(
     listState.OnBottomReached(buffer = 3, onLoadMore = viewModel::loadNextPage)
 
     var searchByGenre by remember { mutableStateOf(initialMediaType != null) }
-    var showMoreFilters by remember { mutableStateOf(false) }
+    var showMoreFilters by rememberSaveable { mutableStateOf(false) }
 
     LaunchedEffect(performSearch.value) {
         if (performSearch.value) {
