8b10dfeef9e1cc2135d4817439473b813ddf0a89
axiel7 <axiel7lopez@gmail.com>
2024-02-08 13:25:09 +0900

    update compose navigation 2.7.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -132,7 +132,7 @@ dependencies {
     implementation("androidx.compose.material3:material3:$materialVersion")
     implementation("androidx.compose.material3:material3-window-size-class:$materialVersion")
 
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.glance:glance-appwidget:1.0.0")
 
     implementation("com.google.accompanist:accompanist-permissions:0.34.0")
