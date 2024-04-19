18ee8ab135fb509e09e1a45c474987ff2e588c87
axiel7 <axiel7lopez@gmail.com>
2024-04-19 17:16:52 +0200

    update datastore 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,7 +119,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycleVersion")
 
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.0")
     implementation("androidx.work:work-runtime:2.9.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
 
