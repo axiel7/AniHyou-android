f3366d0341ee7504421ac2d3dbfc0d613aa275f2
axiel7 <axiel7lopez@gmail.com>
2024-04-19 17:16:11 +0200

    update activity-compose 1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,7 +113,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.activity:activity-compose:1.9.0")
 
     val lifecycleVersion = "2.7.0"
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
