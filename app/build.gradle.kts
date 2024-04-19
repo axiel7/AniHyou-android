cf756e0399877c2cb0d203e014c1a8c56f4f90d5
axiel7 <axiel7lopez@gmail.com>
2024-04-19 17:15:27 +0200

    update androidx.core 1.13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,7 +111,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity-compose:1.8.2")
 
