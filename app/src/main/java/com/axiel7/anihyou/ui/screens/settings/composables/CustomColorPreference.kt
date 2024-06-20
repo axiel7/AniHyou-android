392997e9e79ada53ac8415fcc19e795f80ae5255
axiel7 <axiel7lopez@gmail.com>
2024-06-20 10:33:02 +0200

    feat(Settings): open keyboard on custom color dialog

--- app/src/main/java/com/axiel7/anihyou/ui/screens/settings/composables/CustomColorPreference.kt
+++ app/src/main/java/com/axiel7/anihyou/ui/screens/settings/composables/CustomColorPreference.kt
@@ -14,12 +14,15 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -38,6 +41,8 @@ fun CustomColorPreference(
     var hexValue by remember { mutableStateOf(hexString.drop(1)) }
     var colorValue by remember { mutableStateOf(color) }
     var openDialog by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+
     Row(
         modifier = modifier
             .fillMaxWidth()
@@ -70,6 +75,9 @@ fun CustomColorPreference(
     }
 
     if (openDialog) {
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
+        }
         AlertDialog(
             onDismissRequest = { openDialog = false },
             title = { Text(text = stringResource(R.string.custom_color)) },
@@ -82,6 +90,7 @@ fun CustomColorPreference(
                             colorValue = color
                         }
                     },
+                    modifier = Modifier.focusRequester(focusRequester),
                     label = { Text(text = "HEX") },
                     prefix = { Text(text = "#") },
                 )
