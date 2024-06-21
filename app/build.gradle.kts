500a07288e81ca32bd2e8498e86d549577cad36a
axiel7 <axiel7lopez@gmail.com>
2024-06-21 17:50:39 +0200

    downgrade navigation to 2.8.0-beta02 it was buggy in android 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -129,7 +129,7 @@ dependencies {
     implementation("androidx.compose.material3:material3:$materialVersion")
     implementation("androidx.compose.material3:material3-window-size-class:$materialVersion")
 
-    implementation("androidx.navigation:navigation-compose:2.8.0-beta03")
+    implementation("androidx.navigation:navigation-compose:2.8.0-beta02")
     implementation("androidx.glance:glance-appwidget:1.1.0")
 
     implementation("com.google.accompanist:accompanist-permissions:0.34.0")
