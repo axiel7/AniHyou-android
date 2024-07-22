1b2f513e071aca2fbe36b18e58fa3721c5858534
axiel7 <axiel7lopez@gmail.com>
2024-07-22 19:27:54 +0200

    chore(deps): AGP 8.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     val kotlinVersion = "2.0.0"
-    id("com.android.application") version "8.5.0" apply false
+    id("com.android.application") version "8.5.1" apply false
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion apply false
     id("com.google.devtools.ksp") version "$kotlinVersion-1.0.22" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("com.android.test") version "8.5.0" apply false
+    id("com.android.test") version "8.5.1" apply false
     id("androidx.baselineprofile") version "1.2.4" apply false
 }
