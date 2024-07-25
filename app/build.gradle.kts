b381910c48cbd06907589f753497250a1fbb28e8
axiel7 <axiel7lopez@gmail.com>
2024-07-25 09:39:03 +0200

    chore(deps): navigation 2.8.0-beta06

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -141,7 +141,7 @@ dependencies {
     implementation("androidx.compose.material3:material3:$materialVersion")
     implementation("androidx.compose.material3:material3-window-size-class:$materialVersion")
 
-    implementation("androidx.navigation:navigation-compose:2.8.0-beta05")
+    implementation("androidx.navigation:navigation-compose:2.8.0-beta06")
     implementation("androidx.glance:glance-appwidget:1.1.0")
 
     implementation("com.google.accompanist:accompanist-permissions:0.34.0")
