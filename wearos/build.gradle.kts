dded3948bae376ba9d444da81108c424d4847a80
Axel <axiel7lopez@gmail.com>
2025-03-23 18:57:13 +0100

    build(wear): 2

--- wearos/build.gradle.kts
+++ wearos/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = appPackageName
         minSdk = wearMinSdkVersion
         targetSdk = wearSdkVersion
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
     }
 
