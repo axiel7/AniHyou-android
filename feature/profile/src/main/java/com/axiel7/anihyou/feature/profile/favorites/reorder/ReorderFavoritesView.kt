61ca226fed9aff16d61b3a126c03121ff9b6f326
x8laye4r <contact@mail.x8laye4r.net>
2026-07-31 10:35:02 +0200

    Removed unneeded imports

--- feature/profile/src/main/java/com/axiel7/anihyou/feature/profile/favorites/reorder/ReorderFavoritesView.kt
+++ feature/profile/src/main/java/com/axiel7/anihyou/feature/profile/favorites/reorder/ReorderFavoritesView.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.ExtendedFloatingActionButton
