325abb1957842874114610554ae7cdfa244f01cc
Axel <axiel7lopez@gmail.com>
2026-08-01 15:04:06 +0200

    feat(Search): migrate deprecated SearchBar

--- feature/explore/src/main/java/com/axiel7/anihyou/feature/explore/ExploreSearchBar.kt
+++ feature/explore/src/main/java/com/axiel7/anihyou/feature/explore/ExploreSearchBar.kt
@@ -1,33 +1,34 @@
 package com.axiel7.anihyou.feature.explore
 
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.input.clearText
 import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.AppBarWithSearch
+import androidx.compose.material3.ExpandedFullScreenSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.SearchBarValue
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSearchBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.axiel7.anihyou.core.model.SearchType
 import com.axiel7.anihyou.core.network.type.MediaSort
@@ -39,6 +40,7 @@ import com.axiel7.anihyou.feature.explore.search.SearchContentView
 import com.axiel7.anihyou.feature.explore.search.SearchEvent
 import com.axiel7.anihyou.feature.explore.search.SearchUiState
 import com.axiel7.anihyou.feature.explore.search.SearchViewModel
+import kotlinx.coroutines.launch
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
 
@@ -47,7 +49,9 @@ fun ExploreSearchBar(
     isLoggedIn: Boolean,
     navActionManager: NavActionManager
 ) {
-    val viewModel: SearchViewModel = koinViewModel(parameters = { parametersOf(Routes.Search(), isLoggedIn) })
+    val viewModel: SearchViewModel = koinViewModel {
+        parametersOf(Routes.Search(), isLoggedIn)
+    }
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     ExploreSearchBarContent(
@@ -64,84 +68,95 @@ private fun ExploreSearchBarContent(
     event: SearchEvent?,
     navActionManager: NavActionManager,
 ) {
+    val scope = rememberCoroutineScope()
     val keyboardController = LocalSoftwareKeyboardController.current
     val performSearch = remember { mutableStateOf(false) }
-    var isSearchActive by rememberSaveable { mutableStateOf(false) }
-
-    val searchHorizontalPadding by animateDpAsState(
-        targetValue = if (!isSearchActive) 16.dp else 0.dp,
-        label = "searchHorizontalPadding"
-    )
-    val searchBottomPadding by animateDpAsState(
-        targetValue = if (!isSearchActive) 4.dp else 0.dp,
-        label = "searchBottomPadding"
-    )
+    val searchBarState = rememberSearchBarState()
+    val isSearchExpanded by remember {
+        derivedStateOf {
+            searchBarState.currentValue == SearchBarValue.Expanded
+        }
+    }
     val textFieldState = rememberTextFieldState()
+    val scrollBehavior = SearchBarDefaults.enterAlwaysSearchBarScrollBehavior()
+    val appBarWithSearchColors =
+        SearchBarDefaults.appBarWithSearchColors(
+            searchBarColors = SearchBarDefaults.containedColors(state = searchBarState)
+        )
+
+    LaunchedEffect(isSearchExpanded) {
+        if (!isSearchExpanded) {
+            textFieldState.clearText()
+        }
+    }
+
+    val inputField =
+        @Composable {
+            SearchBarDefaults.InputField(
+                textFieldState = textFieldState,
+                searchBarState = searchBarState,
+                colors = appBarWithSearchColors.searchBarColors.inputFieldColors,
+                onSearch = {
+                    performSearch.value = true
+                    keyboardController?.hide()
+                },
+                placeholder = { Text(text = stringResource(R.string.anime_manga_and_more)) },
+                leadingIcon = {
+                    if (isSearchExpanded) {
+                        IconButton(
+                            onClick = {
+                                scope.launch {
+                                    textFieldState.clearText()
+                                    searchBarState.animateToCollapsed()
+                                }
+                            },
+                            shapes = IconButtonDefaults.shapes()
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.arrow_back_24),
+                                contentDescription = stringResource(R.string.action_back)
+                            )
+                        }
+                    } else {
+                        Icon(
+                            painter = painterResource(R.drawable.search_24),
+                            contentDescription = stringResource(R.string.search)
+                        )
+                    }
+                },
+                trailingIcon = {
+                    if (isSearchExpanded && textFieldState.text.isNotEmpty()) {
+                        IconButton(
+                            onClick = {
+                                textFieldState.clearText()
+                                performSearch.value = true
+                            },
+                            shapes = IconButtonDefaults.shapes()
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.close_24),
+                                contentDescription = stringResource(R.string.delete)
+                            )
+                        }
+                    }
+                }
+            )
+        }
 
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val onActiveChange: (Boolean) -> Unit = {
-            isSearchActive = it
-            if (!isSearchActive) textFieldState.clearText()
-        }
-        SearchBar(
-            inputField = {
-                SearchBarDefaults.InputField(
-                    state = textFieldState,
-                    onSearch = {
-                        performSearch.value = true
-                        keyboardController?.hide()
-                    },
-                    expanded = isSearchActive,
-                    onExpandedChange = onActiveChange,
-                    placeholder = { Text(text = stringResource(R.string.anime_manga_and_more)) },
-                    leadingIcon = {
-                        if (isSearchActive) {
-                            IconButton(
-                                onClick = {
-                                    isSearchActive = false
-                                    textFieldState.clearText()
-                                },
-                                shapes = IconButtonDefaults.shapes()
-                            ) {
-                                Icon(
-                                    painter = painterResource(R.drawable.arrow_back_24),
-                                    contentDescription = stringResource(R.string.action_back)
-                                )
-                            }
-                        } else {
-                            Icon(
-                                painter = painterResource(R.drawable.search_24),
-                                contentDescription = stringResource(R.string.search)
-                            )
-                        }
-                    },
-                    trailingIcon = {
-                        if (isSearchActive && textFieldState.text.isNotEmpty()) {
-                            IconButton(
-                                onClick = {
-                                    textFieldState.clearText()
-                                    performSearch.value = true
-                                },
-                                shapes = IconButtonDefaults.shapes()
-                            ) {
-                                Icon(
-                                    painter = painterResource(R.drawable.close_24),
-                                    contentDescription = stringResource(R.string.delete)
-                                )
-                            }
-                        }
-                    }
-                )
-            },
-            expanded = isSearchActive,
-            onExpandedChange = onActiveChange,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = searchHorizontalPadding)
-                .padding(bottom = searchBottomPadding)
+        AppBarWithSearch(
+            scrollBehavior = scrollBehavior,
+            state = searchBarState,
+            colors = appBarWithSearchColors,
+            inputField = inputField,
+        )
+        ExpandedFullScreenSearchBar(
+            state = searchBarState,
+            inputField = inputField,
+            modifier = Modifier.fillMaxWidth()
         ) {
             SearchContentView(
                 textFieldState = textFieldState,
