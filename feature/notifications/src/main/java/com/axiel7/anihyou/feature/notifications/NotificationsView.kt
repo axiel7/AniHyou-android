4e70c0e50a9bfa656ff8776efcfebd3a346a2d38
Axel <axiel7lopez@gmail.com>
2026-09-08 20:34:28 +0200

    feat(Notifications): initial localization (#259)

--- feature/notifications/src/main/java/com/axiel7/anihyou/feature/notifications/NotificationsView.kt
+++ feature/notifications/src/main/java/com/axiel7/anihyou/feature/notifications/NotificationsView.kt
@@ -28,10 +28,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.axiel7.anihyou.core.model.notification.GenericNotification.Companion.localizedText
 import com.axiel7.anihyou.core.model.notification.NotificationTypeGroup
 import com.axiel7.anihyou.core.network.type.NotificationType
 import com.axiel7.anihyou.core.resources.R
@@ -146,7 +148,7 @@ private fun NotificationsContent(
                     contentType = { it }
                 ) { item ->
                     NotificationItem(
-                        title = item.text,
+                        title = item.localizedText(LocalResources.current),
                         modifier = Modifier.padding(horizontal = 16.dp, vertical = 10.dp),
                         blurImage = blurAdult && item.isAdultMedia,
                         imageUrl = item.imageUrl,
