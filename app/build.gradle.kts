f0a81c348117c5e55ee3675d4018f574fa39a2f7
Axel <axiel7lopez@gmail.com>
2026-09-19 18:45:16 +0200

    feat: enable shrink resource only for gms flavor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ android {
         release {
             isDebuggable = false
             isMinifyEnabled = true
-            isShrinkResources = false
+            isShrinkResources = true
             isCrunchPngs = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -125,6 +125,24 @@ android {
     }
     dependenciesInfo {
         includeInApk = false
+    }
+}
+
+androidComponents {
+    beforeVariants {
+        if (it.buildType == "release" && it.flavorName == "foss") {
+            it.shrinkResources = false
+        }
+    }
+    onVariants {
+        if (it.buildType == "release" && it.flavorName == "gms") {
+            // Disable ABI splits for GMS (fix for building bundle)
+            it.outputs.forEach { output ->
+                if (output.filters.isNotEmpty()) {
+                    output.enabled.set(false)
+                }
+            }
+        }
     }
 }
 
