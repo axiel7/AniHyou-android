341c3e27a81e827bb390de234b8b491f4be48727
x8laye4r <contact@mail.x8laye4r.net>
2026-09-20 15:28:18 +0200

    Changed SimpleMediaSearchSheetView items to MediaItemHorizontal

--- feature/addrecommendation/src/main/java/com/axiel7/anihyou/feature/addrecommendation/search/SimpleMediaSearchSheetView.kt
+++ feature/addrecommendation/src/main/java/com/axiel7/anihyou/feature/addrecommendation/search/SimpleMediaSearchSheetView.kt
@@ -11,10 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.KeyboardActionHandler
 import androidx.compose.foundation.text.input.TextFieldLineLimits
@@ -34,7 +33,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -47,14 +45,15 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.axiel7.anihyou.core.network.SearchMediaQuery
+import com.axiel7.anihyou.core.network.type.MediaFormat
 import com.axiel7.anihyou.core.network.type.MediaType
 import com.axiel7.anihyou.core.resources.R
 import com.axiel7.anihyou.core.ui.common.LocalBlurAdult
 import com.axiel7.anihyou.core.ui.composables.common.singleClick
-import com.axiel7.anihyou.core.ui.composables.media.MEDIA_POSTER_SMALL_WIDTH
-import com.axiel7.anihyou.core.ui.composables.media.MediaItemVertical
-import com.axiel7.anihyou.core.ui.composables.media.MediaItemVerticalPlaceholder
+import com.axiel7.anihyou.core.ui.composables.media.MediaItemHorizontal
+import com.axiel7.anihyou.core.ui.composables.media.MediaItemHorizontalPlaceholder
 import com.axiel7.anihyou.core.ui.composables.sheet.ModalBottomSheet
+import kotlinx.collections.immutable.toImmutableList
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
 
@@ -84,7 +83,7 @@ private fun SearchContent(
     onSelected: (SearchMediaQuery.Medium) -> Unit,
     sheetState: SheetState,
 ) {
-    val listState = rememberLazyGridState()
+    val listState = rememberLazyListState()
     val textFieldState = rememberTextFieldState(initialText = uiState.searchQuery)
     val focusRequester = remember { FocusRequester() }
     val keyboardController = LocalSoftwareKeyboardController.current
@@ -154,8 +153,7 @@ private fun SearchContent(
             Scaffold(
                 Modifier.fillMaxSize()
             ) { padding ->
-                LazyVerticalGrid(
-                    columns = GridCells.Adaptive(minSize = (MEDIA_POSTER_SMALL_WIDTH + 8).dp),
+                LazyColumn (
                     modifier = Modifier.padding(
                         top = padding.calculateTopPadding() + 8.dp,
                         bottom = padding.calculateBottomPadding(),
@@ -164,26 +162,32 @@ private fun SearchContent(
                     ),
                     state = listState,
                     verticalArrangement = Arrangement.spacedBy(8.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
                 ) {
                     if (uiState.isSearching) {
                         items(10) {
-                            MediaItemVerticalPlaceholder()
+                            MediaItemHorizontalPlaceholder()
                         }
                     } else {
                         items(
                             items = uiState.searchResult,
-                            key = { it.id },
-                        ) { item ->
-                            MediaItemVertical(
-                                title = item.basicMediaDetails.title?.userPreferred.orEmpty(),
-                                imageUrl = item.coverImage?.large,
-                                blurImage = blurAdult && item.basicMediaDetails.isAdult == true,
+                            key = { it.id }
+                        ) { media ->
+                            MediaItemHorizontal(
+                                title = media.basicMediaDetails.title?.userPreferred.orEmpty(),
+                                imageUrl = media.coverImage?.large,
+                                blurImage = blurAdult && media.basicMediaDetails.isAdult == true,
+                                score = media.averageScore ?: 0,
+                                format = media.format ?: MediaFormat.UNKNOWN__,
+                                year = media.startDate?.year,
+                                mediaStatus = media.status,
+                                episodes = media.basicMediaDetails.episodes,
+                                chapters = media.basicMediaDetails.chapters,
+                                duration = media.duration,
+                                genres = media.genres?.filterNotNull()?.toImmutableList(),
                                 onClick = {
-                                    onSelected(item)
+                                    onSelected(media)
                                     dismiss()
                                 },
-                                status = item.mediaListEntry?.basicMediaListEntry?.status,
                             )
                         }
                     }
